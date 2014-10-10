3243cf5b8fa698f85d19e9a1010b64056163c47b
megatron-me-uk <github@megatron.me.uk>
2014-10-10 15:42:28 +0100

    improve game, add human and q actor

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import random
-from collections import Counter
+from collections import Counter,deque
 
 def second_largest(numbers):
-    first, second = None, None
+    first, second = -1,-1
     for n in numbers:
         if n > first:
             first, second = n, first
@@ -11,25 +11,118 @@ def second_largest(numbers):
     return second
 
 class simpleState:
-    def __init__(self):
+    def __init__(self,balance):
         self.actions=[0]
-        self.cards=[]
+        self.cards=cardlist()
         self.table=None
-        self.balance=100
+        self.balance=balance
+        self.hand=NoHand()
     def add(self,card):
         self.cards.append(card)
+    def clear(self):
+        self.cards=cardlist()
+        self.hand=NoHand()
 
-class randActor:
-    def __init__(self):
-        self.state=simpleState()
+class dumbActor:
+    def __init__(self,balance=100):
+        self.state=simpleState(balance)
     def chooseAction(self,state):
-        return sample(state.actions)
+        return self.sample(state.actions)
+    def bet(self,maxbet):
+        return maxbet-self.state.inround
+    def pay(self,amnt):
+        self.state.balance+=amnt
+
+class selectActor(dumbActor):
+    def bet(self,maxbet):
+        for cc in self.state.cards:
+            if cc.card>10:
+                return maxbet-self.state.inround
+        if self.state.cards[0].card==self.state.cards[1].card:
+            return maxbet-self.state.inround
+        if self.state.cards[0].suit==self.state.cards[1].suit:
+            return maxbet-self.state.inround
+        return 0
+
+class humanActor(dumbActor):
+    def bet(self,maxbet):
+        print("table:")
+        print(self.state.table)
+        print("hand:")
+        print(self.state.cards)
+        val=-1
+        print("Balance:%f Max in round:%f Your in round:%f"%(self.state.balance,maxbet,self.state.inround))
+        while val<0:
+            try:
+                val=float(input("place bet:"))
+            except ValueError:
+                pass
+        print(val)
+        return val
+    def pay(self,amnt):
+        super().pay(amnt)
+        print(self.state.hand.hand)
+        print("Won:%f"%(amnt,))
+
+class qActor(dumbActor):
+    def __init__(self,balance=100):
+        super().__init__(balance)
+        self.Q=[]
+        self.state.record=[]
+        for i in range(2):
+            Qs=[]
+            rec=[]
+            for j in cards:
+                [Qs.append(random.random()) for k in cards]
+                [rec.append(0) for k in cards]
+            self.Q.append(Qs)
+            self.state.record.append(rec)
+        self.epsilon=0.2
+        self.alpha=0.001
+        self.gamma=0
+        self.state.r=None
+    def bet(self,maxbet):
+        s=self.state.cards[0].card*13+self.state.cards[1].card
+        qbet=self.Q[1][s]
+        qfold=self.Q[0][s]
+        rr=random.random()
+        if qbet>qfold:
+            dec=1-self.epsilon
+        elif qbet<qfold:
+            dec=self.epsilon
+        else:
+            dec=0.5
+        if rr>dec:
+            a=0
+        else:
+            a=1
+        if self.state.r is not None:
+            #if self.Q[a][s]==1337:
+            #    self.Q[a][s]=self.state.r
+            #else:
+            self.Q[self.state.action][self.state.state]+=self.alpha*(self.state.r+self.gamma*self.Q[a][s]-self.Q[self.state.action][self.state.state])
+        self.state.state=s
+        if a==0:
+            self.state.action=0
+            return 0
+        else:
+            self.state.action=1
+            return maxbet-self.state.inround
+    def pay(self,amnt):
+        super().pay(amnt)
+        self.state.record[self.state.action][self.state.state]+=amnt-self.state.inround
+        self.state.r=amnt-self.state.inround
 
 cards=[str(x+2) for x in range(9)]
 cards+=['Jack','Queen','King','Ace']
 suits=['Hearts','Diamonds','Clubs','Spades']
 
-class hand:
+class NoHand:
+    def __init__(self):
+        self.hand=None
+        self.rank=None
+
+class hand(NoHand):
     def __init__(self,cards):
         self.cards=cards
         self.N=len(self.cards)
@@ -106,6 +199,10 @@ class card:
     def __str__(self):
         return "%s of %s" %(cards[self.card],suits[self.suit])
 
+class cardlist(list):
+    def __str__(self):
+        return "||".join([str(x) for x in self])
+
 class deck:
     def __init__(self):
         self.cards=[card(i) for i in range(52)]
@@ -124,10 +221,18 @@ class simpleGame:
             pass
         else:
             pass
-        self.players=[randActor() for _i in range(4)]
-        self.state=simpleState()
+        self.origplayers=[selectActor() for _i in range(7)]+[humanActor()]
+        self.players=deque(self.origplayers)
+        #self.state=simpleState()
         self.deck=deck()
+        self.lb=-2
+        self.bb=-1
+        self.lbv=0.5
+        self.bbv=1
+        self.pot=0
+        self.roundplayers=[]
     def play(self):
+        self.deck=deck()
         self.blinds()
         self.preflop()
         self.betting()
@@ -139,13 +244,42 @@ class simpleGame:
         self.betting()
         self.payWinner()
     def blinds(self):
-        pass
+        self.players.rotate(1)
+        for pp in self.players:
+            pp.state.inround=0
+        self.players[self.lb].state.balance-=self.lbv
+        self.players[self.lb].state.inround+=self.lbv
+        self.pot+=self.lbv
+        self.players[self.bb].state.balance-=self.bbv
+        self.players[self.bb].state.inround+=self.bbv
+        self.pot+=self.bbv
+        self.maxbet=self.bbv
+        self.raiser=self.players[self.bb]
     def preflop(self):
-        self.table=[]
+        self.table=cardlist()
+        self.roundplayers=[]
         for pl in self.players:
             for i in range(2):
                 pl.state.add(self.deck.draw())
             pl.state.table=self.table
+            self.roundplayers.append(pl)
+        maxbet=0
+        while maxbet!=self.maxbet:
+            maxbet=self.maxbet
+            roundplayers=list(self.roundplayers)
+            for pl in roundplayers:
+                if pl==self.raiser:
+                    break
+                amnt=pl.bet(self.maxbet)
+                pl.state.balance-=amnt
+                self.pot+=amnt
+                pl.state.inround+=amnt
+                if pl.state.inround>self.maxbet:
+                    self.maxbet=pl.state.inround
+                    self.raiser=pl
+                elif pl.state.inround<self.maxbet:
+                    self.roundplayers.remove(pl)
+        print(self.roundplayers)
     def flop(self):
         self.table+=[self.deck.draw() for i in range(3)]
     def turn(self):
@@ -155,28 +289,35 @@ class simpleGame:
     def betting(self):
         pass
     def payWinner(self):
-        #for pp in 
-        pass
+        ranks=[]
+        for pp in self.roundplayers:
+            pp.state.hand=hand(self.table+pp.state.cards)
+            ranks.append(pp.state.hand.rank)
+        winners=[]
+        best=min(ranks)
+        for rr,pp in zip(ranks,self.roundplayers):
+            if rr==best:
+                winners.append(pp)
+        val=self.pot
+        print("----")
+        for idx,pp in enumerate(self.players):
+            print(idx,pp)
+            if pp in winners:
+                pp.pay(self.pot/len(winners))
+            else:
+                pp.pay(0)
+        print("====")
+        for pl in self.players:
+            pl.state.clear()
+        self.pot=0
 
 def main():
     g=simpleGame()
-    g.play()
-    ranks=[]
-    for pp in g.players:
-        pp.state.hand=hand(g.table+pp.state.cards)
-        ranks.append(pp.state.hand.rank)
-    print(ranks)
-    winners=[]
-    best=min(ranks)
-    for idx,rr in enumerate(ranks):
-        if rr==best:
-            winners.append(idx)
-    for cc in g.table:
-        print(cc)
-    for ww in winners:
-        print(g.players[ww].state.hand.hand)
-        for cc in g.players[ww].state.cards:
-            print(cc)
+    for i in range(100000):
+        g.play()
+    for pl in g.players:
+        print(pl.state.balance)
+    return g
 
 if __name__=="__main__":
-    main()
+    g=main()
